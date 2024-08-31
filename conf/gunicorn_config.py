686103109e859218abe14688f4af14604c48ed6c
Gabriel <decesar1123@gmail.com>
2024-08-31 13:28:20 -0300

    AN515-54 31/08/2024 13:28:20 Adicionados ao volume database, arquivos log para output de acessos e erros do Gunicorn

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,2 +1,4 @@
 bind = '0.0.0.0:8110'
 workers = 3
+accesslog = '/root/Prodocencia-Django/database/logs/access.log'
+errorlog = '/root/Prodocencia-Django/database/logs/error.log'
--- django_start_app.sh
+++ django_start_app.sh
@@ -1,6 +1,4 @@
-# start gunicorn
-    # cd /root/Prodocencia-Django/
-    # gunicorn -c '/root/conf/gunicorn_config.py' siteProdocencia.wsgi >> /root/.server_logs/gunircorn_main.log 2>&1 &
-    
-    cd /root/Prodocencia-Django/
-    gunicorn -c '/root/conf/gunicorn_config.py' siteProdocencia.wsgi
+cd /root/Prodocencia-Django/
+gunicorn -c '/root/conf/gunicorn_config.py' siteProdocencia.wsgi
+# --error-logfile /root/database/error.log
+# --access-logformat /root/database/access.log
